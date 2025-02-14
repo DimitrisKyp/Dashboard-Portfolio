13e335fbe2c9e56f5dc423f6320a94eeb41067b8
dkiprianou <dkiprianou@olympia-electronics.gr>
2025-02-14 15:45:15 +0200

    background animation implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import NavigationPanel from "./components/NavigationPanel";
 import ProjectionPanel from "./components/ProjectionPanel";
 import MainPanel from "./components/MainPanel";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 export default function App() {
   return (
-    <div className="flex h-screen items-center bg-appBg text-appText">
-      {/* Navigation Links */}
-      <div className="flex h-[80vh] w-[10%] items-center justify-center rounded-sm bg-firstLayer">
-        <NavigationPanel />
-      </div>
-      {/* Display details (slightly bigger height) */}
-      <div className="flex h-[90vh] w-[30%] items-center justify-center rounded-sm bg-secondLayer">
-        <ProjectionPanel />
-      </div>
-      {/* Main Content Area */}
-      <div className="flex h-[80vh] w-[50%] items-center justify-center rounded-sm bg-firstLayer">
-        <MainPanel />
+    <div className="flex h-screen text-appText">
+      {/* Particle Background */}
+      <ParticlesBackground />
+
+      {/* Main Content */}
+      <div className="relative flex w-[90%] items-center">
+        <div className="flex h-[80vh] w-[10%] items-center justify-center rounded-sm bg-firstLayer shadow-md">
+          <NavigationPanel />
+        </div>
+        <div className="flex h-[90vh] w-[30%] items-center justify-center rounded-sm bg-secondLayer shadow-md">
+          <ProjectionPanel />
+        </div>
+        <div className="flex h-[80vh] w-[50%] items-center justify-center rounded-sm bg-firstLayer shadow-md">
+          <MainPanel />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ParticlesBackground.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { type ISourceOptions, MoveDirection } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+
+export default function ParticlesBackground() {
+  const [init, setInit] = useState(false);
+  const [themeColors, setThemeColors] = useState({
+    background: "rgba(3, 11, 21, 1)",
+    particles: "rgba(202, 251, 253, 1)",
+  });
+  const theme = useSelector((state: RootState) => state.settings.theme);
+
+  const updateThemeColors = () => {
+    if (theme === "dark") {
+      setThemeColors({
+        background: "rgba(3, 11, 21, 1)",
+        particles: "rgba(202, 251, 253, 1)",
+      });
+    } else if (theme === "light") {
+      setThemeColors({
+        background: "rgb(245, 227, 206)",
+        particles: "#5a5757",
+      });
+    }
+  };
+
+  useEffect(() => {
+    updateThemeColors();
+    initParticlesEngine(async engine => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, [theme]);
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: themeColors.background,
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "pause",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          repulse: {
+            distance: 50,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        links: {
+          color: themeColors.particles,
+          distance: 200,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.right,
+          enable: true,
+          outModes: {
+            default: "out",
+          },
+          random: true,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 250,
+        },
+        shape: {
+          type: "line",
+        },
+      },
+      detectRetina: true,
+    }),
+    [themeColors],
+  );
+
+  if (init) {
+    return <Particles id="tsparticles" options={options} />;
+  }
+
+  return <></>;
+}
