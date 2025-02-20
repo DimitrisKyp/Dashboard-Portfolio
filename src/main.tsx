92daee9dcb4b515c2a643777b87d604e9fb63a1c
dkiprianou <dkiprianou@olympia-electronics.gr>
2025-02-20 14:20:20 +0200

    Remove notifications and language from store

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { persistor, store } from "./store/store.ts";
 import App from "./App.tsx";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import { NotificationProvider } from "./services/providers/NotificationProvider.tsx";
 import { ThemeProvider } from "./services/providers/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
@@ -13,11 +12,9 @@ createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor!}>
         <ThemeProvider>
-          <NotificationProvider>
-            <App />
-          </NotificationProvider>
+          <App />
         </ThemeProvider>
       </PersistGate>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
--- src/store/slices/preferencesSlice.ts
+++ src/store/slices/preferencesSlice.ts
@@ -1,13 +1,11 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface PreferencesState {
-  language: string;
   theme: "dark" | "light";
   activeMenu: string;
 }
 
 const initialState: PreferencesState = {
-  language: "EN",
   theme: "dark",
   activeMenu: "homeSection",
 };
@@ -16,9 +14,6 @@ const preferencesSlice = createSlice({
   name: "preferences",
   initialState,
   reducers: {
-    setLanguage: (state, action: PayloadAction<string>) => {
-      state.language = action.payload;
-    },
     toggleTheme: state => {
       state.theme = state.theme === "light" ? "dark" : "light";
     },
@@ -28,5 +23,5 @@ const preferencesSlice = createSlice({
   },
 });
 
-export const { setLanguage, toggleTheme, setActiveMenu } = preferencesSlice.actions;
+export const { toggleTheme, setActiveMenu } = preferencesSlice.actions;
 export default preferencesSlice.reducer;
